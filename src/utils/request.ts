15580b615fdbb3ba443a97437136aa3653d5a9b2
buyfakett <buyfakett@vip.qq.com>
2025-05-20 23:06:18 +0800

    增加全局请求参数

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,23 +1,54 @@
 import { createAlova } from 'alova';
 import adapterFetch from 'alova/fetch';
 import ReactHook from 'alova/react';
+import { getToken, removeToken } from "@/src/utils/auth";
+import { Toast } from "@douyinfe/semi-ui";
 
 let ENV_url: string;
 
 try {
-  const { ENV_url: importedUrl } = require('../../url.config');
-  ENV_url = importedUrl;
+    const {ENV_url: importedUrl} = require('../../url.config');
+    ENV_url = importedUrl;
 } catch (error) {
-  // 如果导入文件失败，将 ENV_url 设置为空字符串
-  console.error('没有url.config.js文件:', error);
-  ENV_url = '';
+    // 如果导入文件失败，将 ENV_url 设置为空字符串
+    console.error('没有url.config.js文件:', error);
+    ENV_url = '';
 }
 
+const nonvalidateRequiredApi = [
+    '/api/user/login',
+]
+
 const alovaInstance = createAlova({
-  requestAdapter: adapterFetch(),
-  statesHook: ReactHook,
-  baseURL: ENV_url,
-  responded: response => response.json()
+    requestAdapter: adapterFetch(),
+    statesHook: ReactHook,
+    baseURL: ENV_url,
+    timeout: 30000,
+    beforeRequest(method) {
+        if (!nonvalidateRequiredApi.includes(method.url)) {
+            method.config.headers.token = getToken();
+        }
+    },
+    responded: {
+        onSuccess: async (response, method) => {
+            const json = await response.json();
+            if (json.code !== 200) {
+                // 抛出错误或返回reject状态的Promise实例时，此请求将抛出错误
+                throw new Error(json.msg);
+            } else if (response.status === 401) {
+                // 401 未授权，跳转登录页
+                Toast.error('登录已过期，请重新登录');
+                removeToken();
+                window.location.href = '/login';
+            }
+
+            // 解析的响应数据将传给method实例的transform钩子函数，这些函数将在后续讲解
+            return json.data;
+        },
+        onError: (err, method) => {
+            Toast.error(err.msg)
+        },
+    }
 });
 
 export const request = alovaInstance;
